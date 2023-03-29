2cf16a74e5d77df4fcbfe8f598ffc2838f613263
namira121 <127211008+namira121@users.noreply.github.com>
2023-03-29 10:41:45 +0700

    Add project files.

---
+++ Excercise1_Pemrograman Dasar/Excercise1_Pemrograman Dasar.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int nilaiM, nilaiB, rerata, TotalLulus, TotalGagal;
+	char nama, status;
+
+		cout << "====Nilai====" << endl;
+
+		cout << "Masukkan Nilai Matematika : ";
+		cin >> nilaiM; 
+		cout << "\nMasukkan NIlai Bahasa Inggris : ";
+		cin >> nilaiB;
+
+		rerata = (nilaiM + nilaiB) / 2;
+		if (rerata >= 70 || nilaiM > 80)
+			cout << (status, "LULUS");
+		else
+			cout << (status, "Gagal");
+		
+
+		cout << "\nMasukkan Nama : ";
+		cin >> nama;
+
+		cout << nama << status;
+
+		TotalLulus = 
+		
+		
+
+}
